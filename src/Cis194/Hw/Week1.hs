da9539b312f8c2d042a701f4d6931bb49517b847
Hal Arnold <halarnold2000@yahoo.com>
2014-05-02 22:45:04 -0700

    working stuff

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,13 +5,47 @@ module Cis194.Hw.Week1 where
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+
+--toDigits 0 = []
+--toDigits x = if (x < 0) then [] else 
+--             let last = last' x
+--                 allbut = allButLast x
+--             in (toDigits allbut)++ [last]
+
+--toDigits n = if (n <= 0) then  [] 
+--             else
+--               let last = last' n
+--                   allbut = allButLast n
+--               in (toDigits allbut) ++ [last]          
+
+toDigits n | n <= 0 = []
+           | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]  
+
+-- pick off last digit
+last' :: Integer -> Integer
+last' x = x `mod` 10
+
+-- allButLast 
+allButLast :: Integer -> Integer
+allButLast x = x `div` 10 
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+-- toDigitsRev = reverse . toDigits 
 
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+toDigitsRev n | n <= 0 = []
+              | otherwise =  [last' n] ++ toDigitsRev  (allButLast n)
+
+doubleEveryOther :: [Integer]-> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther all@(_:_:_) = let x = reverse all
+                               in reverse (doubleEveryOther' x) 
+
+
+doubleEveryOther' :: [Integer] -> [Integer]
+--doubleEveryOther' [] = []
+--doubleEveryOther' (x:[]) = [x]
+doubleEveryOther' (x:y:zx) = [x, 2*y] ++ doubleEveryOther' zx
 
 sumDigits :: [Integer] -> Integer
 sumDigits _ = 0
